ec969840208880d80b6970e5c8ea3ffd5a8c0ec9
WuShuHan <909575862@qq.com>
2023-02-15 09:52:31 +0800

    创建新分支

--- models/__init__.py
+++ models/__init__.py
@@ -1,10 +1,12 @@
 from .TRED_GNN2 import TRED_GNN2
 from .TRED_GNN import TRED_GNN
 from .TRED_GNN3 import TRED_GNN3
+from .TRED_GNN4 import TRED_GNN4
 
 
 model_dict = {
     "TRED_GNN2":TRED_GNN2,
     "TRED_GNN":TRED_GNN,
     "TRED_GNN3":TRED_GNN3,
+    "TRED_GNN4":TRED_GNN4
 }
